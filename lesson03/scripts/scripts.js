9d5f8fc0c4fcb88dd0b2b71d1011033482b437e2
Benshirley300 <benshirley300@gmail.com>
2021-05-10 21:46:51 -0600

    weather-site-planning

---
+++ lesson03/scripts/scripts.js
@@ -0,0 +1,4 @@
+let date = new Date();
+let year = date.getFullYear();
+document.getElementById("footer-year").textContent = year;
+document.getElementById("last-update").textContent = document.lastModified;
