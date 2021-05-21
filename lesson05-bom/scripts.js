d13c5ac3de8d2cec856f9a08b33f60cee3021839
Benshirley300 <benshirley300@gmail.com>
2021-05-20 19:17:02 -0600

    book of mormon list

---
+++ lesson05-bom/scripts.js
@@ -0,0 +1,19 @@
+function storeVerse()
+{
+    let verse = document.getElementById("favchap").value;
+    document.getElementById("favchap").value = "";
+    let ul = document.getElementById("list");
+    var li = document.createElement("li");
+    li.innerHTML = verse;
+    var button = document.createElement("button");
+    button.innerHTML = "&#10060";
+    button.onclick = function () {
+        ul.removeChild(this.parentNode);
+        document.getElementById("favchap").focus();
+    };
+    li.appendChild(button);
+    ul.appendChild(li);
+    document.getElementById("favchap").focus();
+}
+
+
