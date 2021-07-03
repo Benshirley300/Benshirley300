254951025f03215782b9c68e0ff746189d565cbb
Benshirley300 <benshirley300@gmail.com>
2021-07-03 09:09:31 -0600

    asdf

---
+++ final-weather/scripts/scripts.js
@@ -0,0 +1,62 @@
+function toggleMenu()
+{
+    document.getElementById("mainNav").classList.toggle("hidden");
+}
+
+let currentDate = new Date();
+
+
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+let dayofWeek = weekday[currentDate.getDay()];
+let dayOfMonth = currentDate.getDate();
+
+var monthName= ["January","February","March","April","May","June","July",
+            "August","September","October","November","December"];
+let month = monthName[currentDate.getMonth()];
+let currentYear = currentDate.getFullYear();
+console.log(currentYear);
+document.getElementById('currentYear').innerHTML = dayofWeek + ", " + dayOfMonth + " " + month + " " + currentYear;
+
+
+
+
+let x = currentDate.getDay();
+for (i = 1; i <= 5; ++i)
+{
+    forecast = document.getElementById("day" + i);
+    forecast.innerText = weekday[x];
+    
+    if (x == 6)
+    {
+      x = 0;
+    }
+    else
+    {
+      ++x;
+    }
+}
+
+if (currentDate.getDay() != 5)
+{
+    document.getElementById("fridayAnnouncement").classList.add("not-friday");
+}
+else
+{
+    document.getElementById("fridayAnnouncement").classList.remove("not-friday");
+}
+
+
+WebFont.load({
+  google: {
+    families: [
+       'Montserrat'
+    ]
+  }
+});
