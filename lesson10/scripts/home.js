21b9d52204be43416ca6463ceebdf9bc56f35364
Benshirley300 <benshirley300@gmail.com>
2021-07-02 19:12:35 -0600

    weather-api

---
+++ lesson10/scripts/home.js
@@ -0,0 +1,74 @@
+let currentDate = new Date();
+
+
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+let dayofWeek = weekday[currentDate.getDay()];
+let dayOfMonth = currentDate.getDate();
+
+var monthName= ["January","February","March","April","May","June","July",
+            "August","September","October","November","December"];
+let month = monthName[currentDate.getMonth()];
+let currentYear = currentDate.getFullYear();
+console.log(currentYear);
+document.getElementById('currentYear').innerHTML = dayofWeek + ", " + dayOfMonth + " " + month + " " + currentYear;
+
+let div = document.getElementById('town-summaries');
+function populateSection(town, passClass)
+{
+    let section = document.createElement('div');
+    section.classList.add(passClass);
+    let img = document.createElement('img');
+    img.setAttribute('src', "images/home-page/" + passClass + ".jpg");
+    section.appendChild(img);
+    let h1 = document.createElement('h1');
+    h1.textContent = town.name;
+    section.appendChild(h1);
+    let h2 = document.createElement('h2');
+    h2.textContent = town.motto;
+    section.appendChild(h2);
+    let p1 = document.createElement('p');
+    p1.textContent = "Year Founded: " + town.yearFounded;
+    section.appendChild(p1);
+    let p2 = document.createElement('p');
+    p2.textContent = "Population: " + town.currentPopulation;
+    section.appendChild(p2);
+    let p3 = document.createElement('p');
+    p3.textContent = "Average Rainfall: " + town.averageRainfall;
+    section.appendChild(p3);
+    div.appendChild(section);
+}
+
+const url = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); 
+    const towns = jsonObject['towns']
+    
+    for (let i = 0; i < towns.length; ++i)
+    {
+        
+        if (towns[i].name == 'Soda Springs')
+        {
+            populateSection(towns[i], "soda-springs");
+        }
+        else if (towns[i].name == 'Preston')
+        {
+            populateSection(towns[i], "preston");
+        }
+        if (towns[i].name == 'Fish Haven')
+        {
+            populateSection(towns[i], "fish-haven");
+        }
+    }
+  });
---
+++ lesson10/scripts/weather-api.js
@@ -0,0 +1,80 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=e86101f673aa05c3dbdbcfb18e2504ae&units=imperial";
+
+
+const today = new Date();
+const y = today.getDay();
+const days = new Array(7);
+days[0] = "Sunday";
+days[1] = "Monday";
+days[2] = "Tuesday";
+days[3] = "Wednesday";
+days[4] = "Thursday";
+days[5] = "Friday";
+days[6] = "Saturday";
+let currentDay = days[y];
+let forecastDay = y;
+
+
+  function populateForecastDay(j, jsObject)
+  {
+      const forecastID = "forecast-" + j;
+      imgSection = document.getElementById(forecastID + "-img");
+      let iconCode = jsObject.weather[0].icon;
+      let iconPath = "http://openweathermap.org/img/w/" + iconCode + ".png";
+      img = document.createElement('img');
+      img.src = iconPath;
+      imgSection.appendChild(img);
+      weatherHigh = document.getElementById(forecastID + "-high");
+      weatherHigh.innerHTML = jsObject.main.temp;
+  }
+
+  let j = 1;
+
+  fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    for (let i = 0; i < 40; ++i)
+    {
+        if (jsObject.list[i].dt_txt.includes("18:00:00"))
+        {
+            populateForecastDay(j, jsObject.list[i]);
+            ++j;
+        }
+    }
+    });
+
+
+const currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=e86101f673aa05c3dbdbcfb18e2504ae&units=imperial";
+fetch(currentWeatherURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    let section = document.getElementById("summary2");
+    section.innerHTML = jsObject.weather[0].main;
+
+    section = document.getElementById("temp");
+    section.textContent = jsObject.main.temp_max;
+
+    section = document.getElementById("summary8");
+    section.innerHTML = jsObject.main.humidity;
+
+    section = document.getElementById("wind");
+    section.textContent = jsObject.wind.speed;
+
+
+    let temp = document.getElementById("temp").textContent;
+    let wind = document.getElementById("wind").textContent;
+    let windChill = document.getElementById("windchill");
+    let windChillContainer = document.getElementById("summary6");
+    value = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp * Math.pow(wind, 0.16));
+    value = Math.round(value);
+    windChill.textContent = value;
+    if (temp < 50 && wind > 3)
+    {
+        value = 35.74 + (0.6215 * temp) - (35.75 * Math.pow(wind, 0.16)) + (0.4275 * temp * Math.pow(wind, 0.16));
+        value = Math.round(value);
+        windChill.textContent = value;
+    }
+    else{
+        windChillContainer.textContent = "N/A";
+    }
+    });
